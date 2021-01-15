385078b91041bb6e3706745d72d964d2d02d4f22
Farhan Khalifa Ibrahim (BRT-SG) <farhan.khalifa@Brtchip.local>
2021-01-15 17:29:07 +0800

    initial commit

---
+++ first-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    const buttonText = 'Click Me!';
+
+    return(
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor:'red',color:'white'}}>{buttonText}</button>
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
